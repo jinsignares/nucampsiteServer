16c4b5f3bf0145f8fb457d70efd0ff605b548cce
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-06-26 10:09:33 -0500

    added promotions and partners routers and schemas

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const Campsite = require('../models/campsite');
-
 const campsiteRouter = express.Router();
-
-campsiteRouter.use(bodyParser.json());
 
 campsiteRouter.route('/')
 .get((req, res, next) => {
