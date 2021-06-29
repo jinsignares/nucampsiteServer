2a2a7c01e7ad8e3adf81d9be7e4cb076b7f0b9a9
foreverajohn <58446479+foreverajohn@users.noreply.github.com>
2021-06-29 15:33:06 -0500

    token authentication

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
-const passport = require('passport')
+const passport = require('passport');
+const authenticate = require('../authenticate');
 
 const router = express.Router();
 
@@ -17,12 +18,12 @@ router.post('/signup', (req, res) => {
       if (err) {
         res.statusCode = 500;
         res.setHeader('Content-Type', 'application/json');
-        res.json({err: err});
+        res.json({ err: err });
       } else {
         passport.authenticate('local')(req, res, () => {
           res.statusCode = 200;
           res.setHeader('Content-Type', 'application/json');
-          res.json({success: true, status: 'Registration Successful!'});
+          res.json({ success: true, status: 'Registration Successful!' });
         })
       }
     }
@@ -30,9 +31,10 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ success: true, status: 'You are successfully logged in!'});
+  res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
 router.get('/logout', (req, res, next) => {
